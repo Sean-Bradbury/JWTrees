b5f8ce2f073fe63bc54ca4adf3d7ed86a2471fa3
sean-bradbury <sean.m.bradbury@googlemail.com>
2020-12-19 11:58:23 +0000

    fixed mobile nav - added js

--- main.js
+++ main.js
@@ -101,3 +101,12 @@ function onScroll(event) {
     }
   });
 }
+
+// Menu show
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
